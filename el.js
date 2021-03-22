598ea64f7b773cf1fe8addad5f1d3f21d749f638
shadoskill <shadoskill@gmail.com>
2021-03-22 06:26:27 -0500

    JavaScript POST Request

--- el.js
+++ el.js
@@ -7,5 +7,15 @@ var el = {
         }
         xhttp.open("GET", url, true);
         xhttp.send();
+    },
+    post:function(url, data, callBack)
+    {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = () => {
+            if(xhttp.readyState == 4) callBack("Post:"+xhttp.response);
+        }
+        xhttp.open("POST", url, true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send(data);
     }
 };
