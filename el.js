a7c6cd9e2e38c46728adaffd566f40c73a83bd40
shadoskill <shadoskill@gmail.com>
2021-03-22 06:40:52 -0500

    Allow POST to have custom content-type

--- el.js
+++ el.js
@@ -8,14 +8,18 @@ var el = {
         xhttp.open("GET", url, true);
         xhttp.send();
     },
-    post:function(url, data, callBack)
+    post:function(url, data, callBack, contentType)
     {
+        let ct = "application/x-www-form-urlencoded";
+        if(contentType !== "undefined"){
+            ct = contentType;
+        }
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = () => {
             if(xhttp.readyState == 4) callBack("Post:"+xhttp.response);
         }
         xhttp.open("POST", url, true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.setRequestHeader("Content-type", ct);
         xhttp.send(data);
     }
 };
