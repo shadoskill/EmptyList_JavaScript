a22d3a279a06a78fe9c6bfe2f8b08478ab52a5ab
shadoskill <shadoskill@gmail.com>
2021-03-23 03:29:47 -0500

    Forogt to remove debug!

--- el.js
+++ el.js
@@ -3,7 +3,7 @@ var el = {
     {
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = () => {
-            if(xhttp.readyState == 4) callBack("Get:"+xhttp.response);
+            if(xhttp.readyState == 4) callBack(xhttp.response);
         }
         xhttp.open("GET", url, true);
         xhttp.send();
@@ -16,7 +16,7 @@ var el = {
         }
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = () => {
-            if(xhttp.readyState == 4) callBack("Post:"+xhttp.response);
+            if(xhttp.readyState == 4) callBack(xhttp.response);
         }
         xhttp.open("POST", url, true);
         xhttp.setRequestHeader("Content-type", ct);
