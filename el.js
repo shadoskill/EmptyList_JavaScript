2c9445b3ca556aa1fed309985707d92fb39f8255
shadoskill <shadoskill@gmail.com>
2021-03-24 01:09:38 -0500

    Add document ready function, cleaned up some formatting.

--- el.js
+++ el.js
@@ -1,25 +1,45 @@
-var el = {
-    get:function(url, callBack)
-    {
+(function(){
+    var constructor = function (selector){
+        this.selector = selector;
+    };
+    constructor.prototype = {
+        on:function(event, fn){
+            const query = document.querySelectorAll(this.selector);
+            query.forEach((element)=>{
+                element.addEventListener(event, fn);
+            });
+            return this;
+        }
+    };
+
+    var el = (selector)=>{
+        return new constructor(selector);
+    };
+    el.ready = (fn)=>{
+        if(typeof fn === 'function'){
+            document.addEventListener("DOMContentLoaded", fn);
+        }        
+    }
+    el.get = (url, fn)=>{
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = () => {
-            if(xhttp.readyState == 4) callBack(xhttp.response);
+            if(xhttp.readyState == 4) fn(xhttp.response);
         }
         xhttp.open("GET", url, true);
         xhttp.send();
-    },
-    post:function(url, data, callBack, contentType)
-    {
+    }
+    el.post = (url, data, fn, contentType)=>{
         let ct = "application/x-www-form-urlencoded";
         if(contentType !== "undefined"){
             ct = contentType;
         }
         let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = () => {
-            if(xhttp.readyState == 4) callBack(xhttp.response);
+        xhttp.onreadystatechange = ()=>{
+            if(xhttp.readyState == 4) fn(xhttp.response);
         }
         xhttp.open("POST", url, true);
         xhttp.setRequestHeader("Content-type", ct);
         xhttp.send(data);
     }
-};
+    window.el = el;
+}());
