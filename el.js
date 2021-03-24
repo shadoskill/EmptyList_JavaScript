834f3c06801834fd3fbef882a6f7641f3e3832a9
shadoskill <shadoskill@gmail.com>
2021-03-24 02:25:50 -0500

    Add CSS, Show, and Hide, show restores last display type.

--- el.js
+++ el.js
@@ -1,3 +1,4 @@
+var showHideMemory = {};
 (function(){
     var constructor = function (selector){
         this.selector = selector;
@@ -8,6 +9,33 @@
             query.forEach((element)=>{
                 element.addEventListener(event, fn);
             });
+            return this;
+        },
+        css:function(args){
+            if(typeof args === 'string'){
+                const query = document.querySelectorAll(this.selector);
+                return query[0].style[args];
+            }
+            else if(typeof args === 'object'){
+                const query = document.querySelectorAll(this.selector);
+                cssArgs = Object.entries(args);
+                query.forEach((element)=>{
+                    cssArgs.forEach(([property, value])=>{
+                        element.style[property] = value;
+                    });
+                });
+                return this;
+            }            
+        },
+        hide:function(){
+            showHideMemory[this.selector] = this.css('display');
+            this.css({'display':'none'});
+            return this;
+        },
+        show:function(){
+            var savedDisplayType = showHideMemory[this.selector];
+            this.css({'display':((savedDisplayType != "")?savedDisplayType:"block")});
+            delete showHideMemory[this.selector];
             return this;
         }
     };
