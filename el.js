acca9cdadb67c24733a1600b511e486fcb09a562
shadoskill <shadoskill@gmail.com>
2021-12-21 17:02:58 -0600

    improve image swap code

--- el.js
+++ el.js
@@ -304,31 +304,50 @@ var alertList = {};
         if(typeof style === "undefined") return false;
         return true;
     }
-    el.inView = function(element, percent){
-        let rect = element.getBoundingClientRect(), windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-        return !(Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100)) < percent || Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percent);
-    }
-    el.imageSwap = function(rate, percent){
-        setInterval(() => {
-            el.each(el(".el-image-swap").query, function(id, element){
-                if(el.inView(element, percent)){
-                    let id = element.id;
-                    el("#"+id).fadeOut();
-                    setTimeout(() => {
-                        element.addEventListener("load", ()=>{
-                            setTimeout(() => {
-                                el("#"+id).fadeIn();
-                            }, 500);
-                        });
-                        setTimeout(() => {
-                            el("#"+id).attr("src", el("#"+id).data("load"));
-                            el("#"+id).removeClass("el-image-swap");
-                        }, 500);
-                    }, 500);
+    // el.inView = function(element, percent){
+    //     let rect = element.getBoundingClientRect(), windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    //     return !(Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100)) < percent || Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percent);
+    // }
+    // el.imageSwap = function(rate, percent){
+    //     setInterval(() => {
+    //         el.each(el(".el-image-swap").query, function(id, element){
+    //             if(el.inView(element, percent)){
+    //                 let id = element.id;
+    //                 el("#"+id).fadeOut();
+    //                 setTimeout(() => {
+    //                     element.addEventListener("load", ()=>{
+    //                         setTimeout(() => {
+    //                             el("#"+id).fadeIn();
+    //                         }, 500);
+    //                     });
+    //                     setTimeout(() => {
+    //                         el("#"+id).attr("src", el("#"+id).data("load"));
+    //                         el("#"+id).removeClass("el-image-swap");
+    //                     }, 500);
+    //                 }, 500);
                     
+    //             }
+    //         }); 
+    //     }, rate);
+    // }
+    el.imageSwap = function(percent){
+        percent /= 100;
+        const observer = new IntersectionObserver(entries=>{
+            el.each(entries, function(entry, k){
+                if(entry.isIntersecting){
+                    entry.target.classList.add("el-fade-out");
+                    entry.target.addEventListener('transitionend', ()=>{
+                        entry.target.src=entry.target.dataset.load;
+                        entry.target.classList.remove("el-fade-out");
+                        entry.target.classList.add("el-fade-in");
+                    });
+                    observer.unobserve(entry.target);                    
                 }
-            }); 
-        }, rate);
+            });
+        },{threshold:percent});
+        el.each(el(".el-image-swap").query, function(id, element){
+            observer.observe(element);
+        });       
     }
     window.el = el;
 }());
